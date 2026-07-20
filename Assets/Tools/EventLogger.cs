f7eb296fa828db622256f33bf71c289bba91d397
Jason Kim <jasonjiseop.kim@gmail.com>
2026-07-20 16:44:33 +0900

    fix: event logger reference

--- Assets/Tools/EventLogger.cs
+++ Assets/Tools/EventLogger.cs
@@ -12,14 +12,13 @@ public class EventLogger : MonoBehaviour
 
     void OnEnable()
     {
-        if (battleController != null)
-            battleState = battleController.BattleState;
-
         // Global events
         EventBus.Subscribe<BattleStartedEvent>(eventData =>
         {
             if (!logEvents) return;
             Debug.Log($"<color=green>[Global]</color>Battle Started: EncounterId={eventData.EncounterId}, Seed={eventData.Seed}, PlayerMoney={eventData.PlayerMoney}, OpponentMoney={eventData.OpponentMoney}");
+            battleState = battleController.BattleState;
+            OnBattleStateInitialized();
         });
         EventBus.Subscribe<BattleEndedEvent>(eventData =>
         {
@@ -37,76 +36,91 @@ public class EventLogger : MonoBehaviour
             Debug.Log($"<color=green>[Global]</color> Run Phase Changed: NewPhase={eventData.Phase}");
         });
 
-        if (battleState != null)
+        
+    }
+
+    void OnBattleStateInitialized()
+    {
+        if (battleState.EventBus != null)
         {
             battleState.EventBus.Subscribe<BattlePhaseChangedEvent>(eventData =>
             {
                 if (!logEvents) return;
-                Debug.Log($"<color=blue>[Battle]</color> Battle Phase Changed: NewPhase={eventData.Phase}");
+                Debug.Log($"<color=red>[Battle]</color> Battle Phase Changed: NewPhase={eventData.Phase}");
             });
 
             battleState.EventBus.Subscribe<ItemUsedEvent>(eventData =>
             {
                 if (!logEvents) return;
-                Debug.Log($"<color=blue>[Battle]</color> Item Used: ItemId={eventData.ItemId}");
+                Debug.Log($"<color=red>[Battle]</color> Item Used: ItemId={eventData.ItemId}");
             });
 
             // Round events
             battleState.EventBus.Subscribe<RoundStartedEvent>(eventData =>
             {
                 if (!logEvents) return;
-                Debug.Log($"<color=blue>[Battle]</color> Round Started: RoundNumber={eventData.RoundNumber}");
+                Debug.Log($"<color=red>[Battle]</color> Round Started: RoundNumber={eventData.RoundNumber}");
             });
 
             battleState.EventBus.Subscribe<RoundEndedEvent>(eventData =>
             {
                 if (!logEvents) return;
-                Debug.Log($"<color=blue>[Battle]</color> Round Ended: RoundNumber={eventData.RoundNumber}");
+                Debug.Log($"<color=red>[Battle]</color> Round Ended: RoundNumber={eventData.RoundNumber}");
             });
 
             // Card events
             battleState.EventBus.Subscribe<CardPlayedEvent>(eventData =>
             {
                 if (!logEvents) return;
-                Debug.Log($"<color=blue>[Battle]</color> Card Played: Card={eventData.Card}");
+                Debug.Log($"<color=red>[Battle]</color> Card Played: Card={eventData.Card}");
             });
             
             battleState.EventBus.Subscribe<HandRefilledEvent>(eventData =>
             {
                 if (!logEvents) return;
-                Debug.Log($"<color=blue>[Battle]</color> Hand Refilled: HandCount={eventData.HandCount}");
+                Debug.Log($"<color=red>[Battle]</color> Hand Refilled: HandCount={eventData.HandCount}");
             });
 
             battleState.EventBus.Subscribe<DeckShuffledEvent>(eventData =>
             {
                 if (!logEvents) return;
-                Debug.Log($"<color=blue>[Battle]</color> Deck Shuffled: ReshuffleCount={eventData.ReshuffleCount}, DeckCount={eventData.DeckCount}");
+                Debug.Log($"<color=red>[Battle]</color> Deck Shuffled: ReshuffleCount={eventData.ReshuffleCount}, DeckCount={eventData.DeckCount}");
             });
 
             // Score events
             battleState.EventBus.Subscribe<ScoreCalculatedEvent>(eventData =>
             {
                 if (!logEvents) return;
-                Debug.Log($"<color=blue>[Battle]</color> Score Calculated: Combatant={eventData.Combatant}, Score={eventData.Score}");
+                Debug.Log($"<color=red>[Battle]</color> Score Calculated: Combatant={eventData.Combatant}, Score={eventData.Score}");
             });
 
             battleState.EventBus.Subscribe<BurstOccurredEvent>(eventData =>
             {
                 if (!logEvents) return;
-                Debug.Log($"<color=blue>[Battle]</color> Burst Occurred: Combatant={eventData.Combatant}, Score={eventData.Score}");
+                Debug.Log($"<color=red>[Battle]</color> Burst Occurred: Combatant={eventData.Combatant}, Score={eventData.Score}");
             });
 
             battleState.EventBus.Subscribe<BurstAttemptedEvent>(eventData =>
             {
                 if (!logEvents) return;
-                Debug.Log($"<color=blue>[Battle]</color> Burst Attempted: Combatant={eventData.Combatant}, Score={eventData.Score}, Threshold={eventData.Threshold}, WillBurst={eventData.WillBurst}");
+                Debug.Log($"<color=red>[Battle]</color> Burst Attempted: Combatant={eventData.Combatant}, Score={eventData.Score}, Threshold={eventData.Threshold}, WillBurst={eventData.WillBurst}");
             });
 
             battleState.EventBus.Subscribe<BlackjackAchievedEvent>(eventData =>
             {
                 if (!logEvents) return;
-                Debug.Log($"<color=blue>[Battle]</color> Blackjack Achieved: Combatant={eventData.Combatant}, Score={eventData.Score}");
+                Debug.Log($"<color=red>[Battle]</color> Blackjack Achieved: Combatant={eventData.Combatant}, Score={eventData.Score}");
             });
+
+            battleState.EventBus.Subscribe<PokerResolvedEvent>(eventData =>
+            {
+                if (!logEvents) return;
+                Debug.Log($"<color=red>[Battle]</color> Poker Resolved: Combatant={eventData.Combatant}, PokerResult={eventData.Poker}");
+            });
+        }
+        else 
+        {
+            Debug.LogWarning("BattleState is null. EventLogger will not subscribe to battle events.");
         }
     }
 }
