578cf54c011a2422fee5554b517b918c73453328
lickelon <jindoman0@naver.com>
2026-07-24 14:53:33 +0900

    feat: wire gameplay effect resolvers

--- Assets/Core/Gameplay/ActiveItemResolver.cs
+++ Assets/Core/Gameplay/ActiveItemResolver.cs
@@ -3,6 +3,13 @@ public static class ActiveItemResolver
     public const string RejectLastHit = "hit_to_played";
     public const string DrawThree = "draw_three";
     public const string DoubleWager = "double_wager";
+    public const string LeverageTriple = "leverage_triple";
+    public const string BurstThresholdPlusThree = "burst_threshold_plus_three";
+    public const string ReturnPlayerFieldCardToHand = "return_player_field_card_to_hand";
+    public const string DrawTwoHearts = "draw_two_hearts";
+    public const string DrawTwoDiamonds = "draw_two_diamonds";
+    public const string DrawTwoClubs = "draw_two_clubs";
+    public const string DrawTwoSpades = "draw_two_spades";
 
     public static bool CanApply(string itemId, BattleState battle)
     {
@@ -14,8 +21,11 @@ public static class ActiveItemResolver
             RejectLastHit => battle.CanDiscardLastPlayerHitCard(),
             DrawThree => battle.CanDrawPlayerCards(),
             DoubleWager => battle.CanDoubleCurrentRoundWager(),
+            LeverageTriple => battle.CanTripleCurrentRoundWager(),
+            BurstThresholdPlusThree => battle.CanIncreasePlayerBurstThreshold(3),
+            ReturnPlayerFieldCardToHand => battle.CanReturnPlayerFieldCardToHand(),
             _ => false
-        };
+        } || TryGetSuitDraw(itemId, out Suit suit) && battle.CanDrawPlayerCards(suit);
     }
 
     public static bool TryApply(string itemId, BattleState battle)
@@ -28,7 +38,32 @@ public static class ActiveItemResolver
             RejectLastHit => battle.DiscardLastPlayerHitCard(),
             DrawThree => battle.DrawCardsToPlayerHand(3) > 0,
             DoubleWager => battle.DoubleCurrentRoundWager(),
+            LeverageTriple => battle.TripleCurrentRoundWager(),
+            BurstThresholdPlusThree => battle.IncreasePlayerBurstThreshold(3),
+            ReturnPlayerFieldCardToHand => battle.ReturnPlayerFieldCardToHand(),
             _ => false
-        };
+        } || TryGetSuitDraw(itemId, out Suit suit) && battle.DrawCardsToPlayerHand(suit, 2) > 0;
+    }
+
+    private static bool TryGetSuitDraw(string itemId, out Suit suit)
+    {
+        switch (itemId)
+        {
+            case DrawTwoHearts:
+                suit = Suit.Hearts;
+                return true;
+            case DrawTwoDiamonds:
+                suit = Suit.Diamonds;
+                return true;
+            case DrawTwoClubs:
+                suit = Suit.Clubs;
+                return true;
+            case DrawTwoSpades:
+                suit = Suit.Spades;
+                return true;
+            default:
+                suit = default;
+                return false;
+        }
     }
 }
--- Assets/Core/Gameplay/CardModifierResolver.cs
+++ Assets/Core/Gameplay/CardModifierResolver.cs
@@ -1,3 +1,5 @@
+using System;
+
 public static class CardModifierResolver
 {
     public const string RankToHearts = "rank_to_hearts";
@@ -6,10 +8,25 @@ public static class CardModifierResolver
     public const string RankToSpades = "rank_to_spades";
     public const string NegativeRank = "negative_rank";
     public const string HitLower = "hit_lower";
+    public const string HitHigher = "hit_higher";
     public const string DrawSuit = "draw_suit";
+    public const string DrawRank = "draw_rank";
     public const string MoveJack = "move_jack";
+    public const string MoveOpponentToPlayer = "move_opponent_to_player";
+    public const string DemoteOpponentCard = "demote_opponent_card";
     public const string CopyQueen = "copy_queen";
+    public const string OutsourceOpponentCard = "outsource_opponent_card";
     public const string DuplicateKing = "duplicate_king";
+    public const string DuplicateOpponentCard = "duplicate_opponent_card";
+    public const string HitTopDeckCard = "hit_top_deck_card";
+    public const string SplitPreviousCard = "split_previous_card";
+    public const string DoubleCardValue = "double_card_value";
+    public const string HalfCardValue = "half_card_value";
+    public const string TriggerPreviousEffect = "trigger_previous_effect";
+    public const string DiscardLowestNumber = "discard_lowest_number";
+    public const string JokerValue = "joker_value";
+    public const string JokerValuePrefix = "joker_value_";
+    public const string ZeroThenForceHit = "zero_then_force_hit";
 
     public static Card Apply(RunCard runCard, string modifierId)
     {
@@ -39,5 +56,24 @@ public static class CardModifierResolver
             RankToSpades => new Card(Suit.Spades, card.Rank, modifierId),
             _ => new Card(card.Suit, card.Rank, modifierId)
         };
+    }
+
+    public static string CreateResolvedJokerValue(int value)
+    {
+        return $"{JokerValuePrefix}{value}";
+    }
+
+    public static bool TryGetResolvedJokerValue(string modifierId, out int value)
+    {
+        if (!string.IsNullOrWhiteSpace(modifierId)
+            && modifierId.StartsWith(JokerValuePrefix, StringComparison.Ordinal)
+            && int.TryParse(modifierId.Substring(JokerValuePrefix.Length), out value))
+        {
+            if (value == 1 || value == 2 || value == 12 || value == 21)
+                return true;
+        }
+
+        value = 0;
+        return false;
     }
 }
