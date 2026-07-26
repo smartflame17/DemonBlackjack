af3c51b6165645c31b1ab9f016b1d87bd6720093
Jason Kim <jasonjiseop.kim@gmail.com>
2026-07-26 14:43:30 +0900

    fix: nunit test making build fail

--- Assets/Tests/EditMode/DeckViewPanelTests.cs
+++ Assets/Tests/EditMode/DeckViewPanelTests.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR && UNITY_INCLUDE_TESTS
 using System.Reflection;
 using NUnit.Framework;
 using UnityEngine;
@@ -74,3 +75,4 @@ public sealed class DeckViewPanelTests
         field.SetValue(panel, value);
     }
 }
+#endif
